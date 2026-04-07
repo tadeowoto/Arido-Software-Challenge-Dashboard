fafeda4d45537d687fe4f544e5be60b13049e194
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-07 12:06:40 -0300

    feat(home-page): componentized header and styles added to it

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,11 @@ import Table from "@/components/Table";
 import FilterForm from "@/components/FilterForm";
 import userResponse from "@/mocks/userResponse.json";
 import { USER_COLUMNS } from "@/data/data";
+import Header from "@/components/layout/Header";
 export default function Home() {
   return (
     <section className="w-full min-h-screen overflow-auto h-fit bg-bg-dark text-white flex flex-col">
-      <header className=" sticky top-0 z-50 w-full h-16 bg-surface-dark flex items-center justify-between p-5">
-        <div>
-          <h1>User Dashboard Arido Software</h1>
-        </div>
-        <div>
-          <button>Create User</button>
-        </div>
-      </header>
+      <Header />
       <section className="w-full flex  items-center justify-between p-4">
         <FilterForm />
       </section>
