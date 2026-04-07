fcc211403178f6b86c15b656be1aa41e24b1fcc9
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-07 19:15:02 -0300

    feat(UserFormModal): main structore added and modal implementation

---
+++ components/UserFormModal.tsx
@@ -0,0 +1,92 @@
+import { X, User, Lock, Shield } from "lucide-react";
+
+interface UserFormModalProps {
+  open: boolean;
+  onclose: () => void;
+}
+
+export default function UserFormModal({ open, onclose }: UserFormModalProps) {
+  if (!open) return null;
+
+  return (
+    <div className="fixed inset-0 z-100 flex items-center justify-center p-4">
+      <div
+        className="absolute inset-0 bg-slate-900/60 backdrop-blur-sm transition-opacity"
+        onClick={onclose}
+      />
+
+      <div className="relative bg-white w-full max-w-lg rounded-2xl shadow-2xl overflow-hidden animate-in fade-in zoom-in duration-200">
+        <div className="flex items-center justify-between p-6 border-b border-slate-100">
+          <div>
+            <h2 className="text-xl font-bold text-slate-800">
+              Crear Nuevo Usuario
+            </h2>
+            <p className="text-sm text-slate-500 mt-1">
+              Por favor, ingrese los detalles del nuevo usuario.
+            </p>
+          </div>
+          <button
+            onClick={onclose}
+            className="p-2 hover:bg-slate-100 rounded-full transition-colors text-slate-400 hover:text-slate-600"
+          >
+            <X className="w-5 h-5" />
+          </button>
+        </div>
+
+        <form className="p-6 space-y-5">
+          <div className="space-y-1.5">
+            <label className="text-sm font-semibold text-slate-700">
+              Nombre de Usuario
+            </label>
+            <div className="relative">
+              <User className="absolute left-3 top-3 w-4 h-4 text-slate-400" />
+              <input
+                type="text"
+                placeholder="Pedro Pérez..."
+                className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-sky-500/20 focus:border-sky-500 transition-all text-slate-900"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-1.5">
+            <label className="text-sm font-semibold text-slate-700">
+              Password
+            </label>
+            <div className="relative">
+              <Lock className="absolute left-3 top-3 w-4 h-4 text-slate-400" />
+              <input
+                type="password"
+                placeholder="********"
+                className="w-full pl-10 pr-4 py-2.5 bg-slate-50 border border-slate-200 rounded-xl text-sm focus:outline-none focus:ring-2 focus:ring-sky-500/20 focus:border-sky-500 transition-all text-slate-900"
+              />
+            </div>
+          </div>
+
+          <div className="space-y-3 pt-2">
+            <div className="flex items-center gap-2 text-slate-700">
+              <Shield className="w-4 h-4" />
+              <label className="text-sm font-semibold">
+                Security Groups & Access Levels
+              </label>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center p-3 bg-slate-50 rounded-xl border border-slate-100">
+            <select className="  flex-1 text-sm focus:outline-none text-slate-700">
+              <option>Maintenance</option>
+            </select>
+            <select className="w-24  text-black border border-slate-200 rounded-lg py-1 px-2 text-xs focus:outline-none">
+              <option>Read</option>
+              <option>Write</option>
+              <option>Admin</option>
+              <option>Owner</option>
+            </select>
+          </div>
+
+          <button className="w-full py-3 bg-[#0ea5e9] hover:bg-[#0284c7] text-white rounded-xl font-bold text-sm shadow-lg shadow-sky-500/20 transition-all active:scale-[0.98] mt-4">
+            Create User
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,19 +1,31 @@
+"use client";
 import { Plus } from "lucide-react";
+import { useState } from "react";
+import UserFormModal from "../UserFormModal";
 
 interface HeaderProps {
   where: string;
 }
 
 export default function Header({ where }: HeaderProps) {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   return (
     <header className="font-bold sticky top-0 z-50 w-full h-16 bg-surface-dark flex items-center justify-between p-5">
       <div>
         <h1 className="text-xl md:text-2xl ">{where}</h1>
       </div>
-      <button className="flex items-center gap-2 bg-[#0ea5e9] hover:bg-[#0284c7] text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors shadow-sm active:scale-95">
+      <button
+        onClick={() => setIsFormOpen(true)}
+        className="flex items-center gap-2 bg-[#0ea5e9] hover:bg-[#0284c7] text-white px-4 py-2 rounded-lg text-sm font-medium transition-colors shadow-sm active:scale-95"
+      >
         <Plus className="w-4 h-4" />
         Create User
       </button>
+
+      {isFormOpen && (
+        <UserFormModal open={isFormOpen} onclose={() => setIsFormOpen(false)} />
+      )}
     </header>
   );
 }
