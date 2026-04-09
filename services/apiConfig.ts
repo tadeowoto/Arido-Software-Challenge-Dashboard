346fda0451dcfaca0ab7f785b709668867d39e51
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-04-09 09:28:52 -0300

    feat(services): api generic fetch function added

---
+++ services/apiConfig.ts
@@ -0,0 +1,19 @@
+
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000/api";
+
+export const apiFetch = async(endpoint: string, options?: RequestInit) => {
+    const response = await fetch(`${API_BASE_URL}/${endpoint}`, {
+        ...options,
+        headers: {
+            "Content-Type": "application/json",
+            ...(options?.headers || {}),
+        },
+    });
+
+    if (!response.ok) {
+    const error = await response.json().catch(() => ({ message: "Error desconocido" }));
+    throw new Error(error.message || "Error en la petición");
+  }
+
+    return response.json();
+}
